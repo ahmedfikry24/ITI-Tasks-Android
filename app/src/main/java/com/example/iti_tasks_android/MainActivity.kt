cec1b131f346b0707f2aa741ea4a755999ee9518
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-07-29 19:18:15 +0300

    implement task 2

--- app/src/main/java/com/example/iti_tasks_android/MainActivity.kt
+++ app/src/main/java/com/example/iti_tasks_android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.iti_tasks_android
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,10 +13,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var phoneNum: AppCompatEditText
-    private lateinit var message: AppCompatEditText
-    private lateinit var closeButton: AppCompatButton
-    private lateinit var nextButton: AppCompatButton
+    private lateinit var firstShape: LinearLayout
+    private lateinit var secondShape: LinearLayout
+    private lateinit var thirdShape: LinearLayout
+    private lateinit var fourthShape: LinearLayout
+    private lateinit var fifthShape: LinearLayout
+    private lateinit var firstButton: AppCompatButton
+    private lateinit var secondButton: AppCompatButton
+    private lateinit var thirdButton: AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,21 +35,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
-        phoneNum = findViewById(R.id.mobile_edit_text)
-        message = findViewById(R.id.message_edit_text)
-        closeButton = findViewById(R.id.close_button)
-        nextButton = findViewById(R.id.next_button)
+        firstShape = findViewById(R.id.first_linear)
+        secondShape = findViewById(R.id.second_linear)
+        thirdShape = findViewById(R.id.third_linear)
+        fourthShape = findViewById(R.id.fourth_linear)
+        fifthShape = findViewById(R.id.fifth_linear)
+        firstButton = findViewById(R.id.first_button)
+        secondButton = findViewById(R.id.second_button)
+        thirdButton = findViewById(R.id.third_button)
     }
 
     private fun initOnClickListeners() {
-        closeButton.setOnClickListener {
-
+        var thirdShapeColor = R.color.blue
+        var fourthShapeColor = R.color.orange
+        var fifthShapeColor = R.color.green
+        firstShape.setOnClickListener {
+            firstShape.background = getDrawable(R.color.purple)
         }
-        nextButton.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            intent.putExtra("phone", phoneNum.text.toString())
-            intent.putExtra("message", message.text.toString())
-            startActivity(intent)
+        secondShape.setOnClickListener {
+            secondShape.background = getDrawable(R.color.black)
+        }
+        thirdShape.setOnClickListener {
+            thirdShape.background = getDrawable(thirdShapeColor)
+        }
+        fourthShape.setOnClickListener {
+            fourthShape.background = getDrawable(fourthShapeColor)
+        }
+        fifthShape.setOnClickListener {
+            fifthShape.background = getDrawable(fifthShapeColor)
+        }
+        firstButton.setOnClickListener {
+            thirdShapeColor = R.color.green
+            fourthShapeColor = R.color.green
+            fifthShapeColor = R.color.green
+        }
+        secondButton.setOnClickListener {
+            thirdShapeColor = R.color.orange
+            fourthShapeColor = R.color.orange
+            fifthShapeColor = R.color.orange
+        }
+        thirdButton.setOnClickListener {
+            thirdShapeColor = R.color.purple
+            fourthShapeColor = R.color.purple
+            fifthShapeColor = R.color.purple
         }
     }
 }
