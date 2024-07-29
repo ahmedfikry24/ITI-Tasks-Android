09774b07067c2804e60267bf3e7a8ed70d3fd8a2
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-07-29 18:33:51 +0300

    implement task

--- app/src/main/java/com/example/iti_tasks_android/MainActivity.kt
+++ app/src/main/java/com/example/iti_tasks_android/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.iti_tasks_android
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var phoneNum: AppCompatEditText
+    private lateinit var message: AppCompatEditText
+    private lateinit var closeButton: AppCompatButton
+    private lateinit var nextButton: AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +24,27 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initViews()
+        initOnClickListeners()
+    }
+
+    private fun initViews() {
+        phoneNum = findViewById(R.id.mobile_edit_text)
+        message = findViewById(R.id.message_edit_text)
+        closeButton = findViewById(R.id.close_button)
+        nextButton = findViewById(R.id.next_button)
+    }
+
+    private fun initOnClickListeners() {
+        closeButton.setOnClickListener {
+
+        }
+        nextButton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("phone", phoneNum.text.toString())
+            intent.putExtra("message", message.text.toString())
+            startActivity(intent)
         }
     }
 }
