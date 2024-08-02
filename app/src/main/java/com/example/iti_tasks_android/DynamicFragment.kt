47ef620926c6974925ef50a33453f2446881c8ad
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-02 15:18:11 +0300

    refactor task

--- app/src/main/java/com/example/iti_tasks_android/DynamicFragment.kt
+++ app/src/main/java/com/example/iti_tasks_android/DynamicFragment.kt
@@ -1,50 +1,73 @@
 package com.example.iti_tasks_android
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+private const val TAG_DYNAMIC = "dynamicFragment"
 
 class DynamicFragment : Fragment() {
 
+    override fun onAttach(context: Context) {
+        Log.e(TAG_DYNAMIC, "onAttach: ")
+        super.onAttach(context)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.e(TAG_DYNAMIC, "onCreate: ")
         super.onCreate(savedInstanceState)
-        Log.e("lifecycle 1", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.e("lifecycle 2", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        Log.e(TAG_DYNAMIC, "onCreateView: ")
         return inflater.inflate(R.layout.fragment_dynamic, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.e(TAG_DYNAMIC, "onViewCreated: ")
+        super.onViewCreated(view, savedInstanceState)
+    }
+
     override fun onStart() {
+        Log.e(TAG_DYNAMIC, "onStart: ")
         super.onStart()
-        Log.e("lifecycle 3", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     }
 
     override fun onResume() {
+        Log.e(TAG_DYNAMIC, "onResume: ")
         super.onResume()
-        Log.e("lifecycle 4", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     }
 
     override fun onPause() {
+        Log.e(TAG_DYNAMIC, "onPause: ")
         super.onPause()
-        Log.e("lifecycle 5", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     }
 
     override fun onStop() {
+        Log.e(TAG_DYNAMIC, "onStop: ")
         super.onStop()
-        Log.e("lifecycle 6", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    }
+
+    override fun onDestroyView() {
+        Log.e(TAG_DYNAMIC, "onDestroyView: ")
+        super.onDestroyView()
     }
 
     override fun onDestroy() {
+        Log.e(TAG_DYNAMIC, "onDestroy: ")
         super.onDestroy()
-        Log.e("lifecycle 7", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    }
+
+    override fun onDetach() {
+        Log.e(TAG_DYNAMIC, "onDetach: ")
+        super.onDetach()
     }
 
 }
