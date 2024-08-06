58bff3ab8a3e65de71e5df6f514433d4125eba03
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-08-06 19:11:21 +0300

    implement navDrawer

---
+++ app/src/main/java/com/example/iti_tasks_android/ui/SlideShowFragment.kt
@@ -0,0 +1,18 @@
+package com.example.iti_tasks_android.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.iti_tasks_android.R
+
+class SlideShowFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_slide_show, container, false)
+    }
+}
